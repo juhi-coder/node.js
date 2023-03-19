cec53227ad41025d4159d161966c78d79ee16149
juhi-coder <kumarijuhimdb2@gmail.com>
2023-03-19 16:26:08 +0530

    same

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http= require("http");
 const server=http.createServer((req,res)=>{
-    res.end("hello from the other side")
+    res.end("hello from the juhi side")
 });
 
 server.listen(4000,'127.0.0.1',()=>{
