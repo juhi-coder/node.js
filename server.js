5092da385190fe2563743bce500651729830d7e3
juhi-coder <kumarijuhimdb2@gmail.com>
2023-03-20 14:00:34 +0530

    Q-8(I want you to read all the messages from the file and show it at the top of the form. Everytime you add a new message it should show at the top of the form.)

--- server.js
+++ server.js
@@ -1,27 +1,50 @@
-const http= require("http");
-const server=http.createServer((req,res)=>{
-const url=req.url;
-if(url === '/')
-{
-    res.write("hi this is juhi besides here");
-    return res.end();
-}
-else if(url==="/home")
-{
-    res.write(" Welcome home");
-    return res.end();
-}
-else if(url==="/about")
-{
-    res.write("Welcome to About Us page");
-    return res.end();
-}
-else if(url==="/node")
-{
-    res.write("Welcome to my Node Js project");
-    return res.end();
-}
-})
-server.listen(8081,()=>{
-    console.log('juhi kumari')
-})
+const http = require("http");
+const fs = require('fs');
+
+const server = http.createServer((req, res) =>{
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        fs.readFile("message.txt",{encoding:"utf-8"},(err,data)=>{
+        if(err)
+        {
+            console.log(err);
+        }
+        console.log(`data is`+data);
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write(`<body>${data}</body>`)
+        res.write(`<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>`);
+        return res.end();
+    });
+    }
+
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+
+        return req.on('end', () => {
+            const parseBody = Buffer.concat(body).toString();
+            const message = parseBody.split("=")[1];
+            fs.writeFile('message.txt', message,(err) =>{
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+            
+        });
+    };
+
+    res.setHeader('content-type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>')
+    res.write('<body><h1>Hello From Juhi Side</h1></body>')
+    res.write('</html>');
+    res.end();
+});
+
+server.listen(3000);
